f8ea34abd43dbd8385738f279c930b5e5f46f9fd
Ritwik Gupta <ritwikg.0501@gmail.com>
2026-02-10 17:31:46 +0530

    Initial setup: build system and dependencies

---
+++ build/CMakeFiles/swarm-sim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/swarm-sim.dir/link.d"
+  "CMakeFiles/swarm-sim.dir/src/main.cpp.o"
+  "CMakeFiles/swarm-sim.dir/src/main.cpp.o.d"
+  "swarm-sim"
+  "swarm-sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/swarm-sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Swarm Simulator - Build System Test" << std::endl;
+  return 0;
+}
